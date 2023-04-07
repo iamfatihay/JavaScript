a1f0eb252524bd91d705780dd6d1563949e7ffb4
Fatih <16fay61@gmail.com>
2023-04-07 16:30:50 +0200

    aa

--- class-notes/DE-04-functions/1-functionDecleration.js
+++ class-notes/DE-04-functions/1-functionDecleration.js
@@ -8,7 +8,25 @@
 console.log("************ 1- FUNC DECLARATION *************");
 
 //*Örnek1:****************************************************************/
+//?Fonksiyonun tanımlanması
 
+// yazdir()
+
+function yazdir(){
+
+    console.log("e daha daha nasılsınız");
+}
+ yazdir() // call veya invoke ( fonksiyonu çağırmak)
+
+//* */ Ornek2************PARAMETRELİ FONKSİYON********************
+function adYazdir(ad, ikinciAd, soyad){
+
+    console.log(soyad, ad,ikinciAd );
+}
+
+adYazdir("kubilay", "ozlem","NİHAL")
+
+//* Ornek3********* parametreli, dönüş değerli****************
 
 function yasHesapla(ad,tarih){
 
@@ -20,9 +38,24 @@ const ipek=yasHesapla("ipek", 1979);
 const yunus=yasHesapla("yunus",1990)
 const fatih=yasHesapla("fatih",2000)
 
-console.log((ipek+yunus+fatih)/3);
+console.log(ipek,yunus, fatih);
 
+
+console.log((ipek+yunus+fatih)/3);
 
 //* Örnek4: Parametreli, Dönüş değerli
 //**********************************************************/
-//! Function declaration yöntemi ile  fonksiyonun tanımlanması çağrılmasında önce veya sonra olabilir.
+//! Function declaration yöntemi ile  fonksiyonun tanımlanması çağrılmasından önce veya sonra olabilir.
+
+//tek çift sorgulaması
+
+ console.log(tekCiftSayi(5));  
+ console.log(tekCiftSayi(50));  
+
+function tekCiftSayi(sayi){
+return  sayi%2==0 ? "cifttir" : "tektir"
+
+}
+
+
+
--- class-notes/DE-04-functions/2-functionExpression.js
+++ class-notes/DE-04-functions/2-functionExpression.js
@@ -12,28 +12,32 @@
 //**********************************************************/
 console.log("******** 2- EXPRESSION*******");
 
-//once fonksiyon olusturulur (sart)
+//önce fonksiyon oluşturulur (şart)
 
-const tekCift
+const tekCift = function (sayi) {
+  return sayi % 2 == 0 ? "cifttir " : "tektir";
+};
+
+alert(tekCift(15));
 
 
-
-
+//* Örnek2:en büyük sayıyı bul
+//**********************************************************/
 
 const buyukBul=function(x,y,z){
-    let enBuyuk;
-    
-        if(x>y && x>z){
-            enBuyuk=x
-        }else if(y>x && y>z){
-            enBuyuk=y
-        }else {
-            enBuyuk=z
-        }
-    
-    return enBuyuk
-    
-    
+let enBuyuk;
+
+    if(x>y && x>z){
+        enBuyuk=x
+    }else if(y>x && y>z){
+        enBuyuk=y
+    }else {
+        enBuyuk=z
     }
-    
-    alert(buyukBul(3,5,9))
+
+return enBuyuk
+
+
+}
+
+alert(buyukBul(3,5,9))
