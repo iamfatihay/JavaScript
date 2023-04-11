5532275d7b185189720b3e7a4a5044f8a2e58bba
Fatih <16fay61@gmail.com>
2023-04-11 12:16:15 +0200

    aall

--- class-notes/DE-04-functions/4-scope.js
+++ class-notes/DE-04-functions/4-scope.js
@@ -5,19 +5,19 @@ console.log("************ 4- SCOPE *************");
 
 //! Global Scope
 
-let birinci =5;
-let ikinci =12;
-let ucuncu =123;
+let birinci = 5;
+let ikinci = 12;
+let ucuncu = 123;
 
-console.log(birinci,ikinci,ucuncu);
+console.log(birinci, ikinci, ucuncu);
 
-const scopeDeneme=function(){
-    birinci=8
-let ikinci=45
-console.log(ikinci);
+const scopeDeneme = function () {
+    birinci = 8
+    let ikinci = 45
+    console.log(ikinci);
 
-let dorduncu=1234
-// return dorduncu => yazılır fonksiyon dışına dorduncu yandaki gibi yazarsak dönmüş olur=>  console.log(scopeDeneme())=1234
+    let dorduncu = 1234
+    // return dorduncu => yazılır fonksiyon dışına dorduncu yandaki gibi yazarsak dönmüş olur=>  console.log(scopeDeneme())=1234
 }
 
 scopeDeneme()
