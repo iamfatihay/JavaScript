0bfcee0a14ec4acd67212ab44608fbfc28197af7
Fatih <16fay61@gmail.com>
2023-04-13 12:25:02 +0200

    13.04.23 ders sonu

--- class-notes/DE-07-Arrays-Iteration/1-array-loop.js
+++ class-notes/DE-07-Arrays-Iteration/1-array-loop.js
@@ -7,20 +7,20 @@
 //* ======================================================
 //*ÖRNEK: dizi1 arrayindeki pozitif sayılarla negatif sayıları 2 ayrı array de toplayan function yazınız
 
-const dizi1=[-5,15,22,-4,45,79,-54]
+const dizi1 = [-5, 15, 22, -4, 45, 79, -54]
 
-const negatifler1=[]
-const pozitifler1=[]
+const negatifler1 = []
+const pozitifler1 = []
 
-const dizilereAyir1 =()=>{
+const dizilereAyir1 = () => {
 
     for (let i = 0; i < dizi1.length; i++) {
         if (dizi1[i] < 0) {
             negatifler1.push(dizi1[i])
-        }else pozitifler1.push(dizi1[i])     
+        } else pozitifler1.push(dizi1[i])
     }
-console.log(negatifler1);
-console.log(pozitifler1);
+    console.log(negatifler1);
+    console.log(pozitifler1);
 }
 
 dizilereAyir1()
@@ -28,40 +28,40 @@ dizilereAyir1()
 //* ======================================================
 //*                     FOR IN
 //* ======================================================
-const dizi2=[-5,15,22,-4,45,79,-54]
+const dizi2 = [-5, 15, 22, -4, 45, 79, -54]
 
-const negatifler2=[]
-const pozitifler2=[]
+const negatifler2 = []
+const pozitifler2 = []
 
-const dizilereAyir2 =()=>{
+const dizilereAyir2 = () => {
 
     for (let i in dizi2) {
         if (dizi2[i] < 0) {
             negatifler2.push(dizi2[i])
-        }else pozitifler2.push(dizi2[i])     
+        } else pozitifler2.push(dizi2[i])
     }
-console.log(negatifler2);
-console.log(pozitifler2);
+    console.log(negatifler2);
+    console.log(pozitifler2);
 }
 
 dizilereAyir2()
 //* ======================================================
 //*                     FOR OF
 //* ======================================================
-const dizi3=[-5,15,22,-4,45,79,-54]
+const dizi3 = [-5, 15, 22, -4, 45, 79, -54]
 
-const negatifler3=[]
-const pozitifler3=[]
+const negatifler3 = []
+const pozitifler3 = []
 
-const dizilereAyir3 =()=>{
+const dizilereAyir3 = () => {
 
     for (let i of dizi3) {
         if (i < 0) {
             negatifler3.push(i)
-        }else pozitifler3.push(i)     
+        } else pozitifler3.push(i)
     }
-console.log(negatifler3);
-console.log(pozitifler3);
+    console.log(negatifler3);
+    console.log(pozitifler3);
 }
 
 dizilereAyir3()
@@ -75,14 +75,39 @@ dizilereAyir3()
 //*  ana programa döndürecek fonksiyonu yazınız. 
 //*--------------------------------------------------------
 
-const animal=["fil", "aslan", "deve","fil", "kaplan","fil","deve", "aslan", "aslan", "aslan"]
-let sayac = 0
-const userAnimal=prompt("Lutfen bir hayvan adi giriniz: ")
+// const animal=["fil", "aslan", "deve","fil", "kaplan","fil","deve", "aslan", "aslan", "aslan"]
+// const userAnimal=prompt("Lutfen bir hayvan adi giriniz: ")
 
-const findAnimal=()=>{
-    
+// const findAnimal=()=>{
+//     let sayac = 0
+//     for (let i in animal) {
+//         if (animal[i]==userAnimal) sayac++  
+//         }
+//         return `${userAnimal} hayvanindan ${sayac} adet bulunmaktadir`
+//     }
+
+// alert(findAnimal());
+
+//! global de tanımlanan değişkenleri fonksiyon içinde kullanacaksam, fonksiyona parametreyle göndermem zorunlu değildir,
+//! zaten süslü içinden globaldekilere erişebilirim, parametre ile göndereceksem de,
+//! fonksiyon içinde onlara farklı isim takabilirim yani animal dizisini gönderip a diye karşılayıp,
+//! fonksiyon içinde a ismiyle kullanabilirim
 
 
+//* for of dongusu, for in dongusunun bir cok veri yapisini kapsayacak sekilde guncellenmis halidir.
+//* Bu dongude dizi icindeki veriye erisirken indisleme kullanmaya gerek yoktur.
+//* ORNEK: alttaki dizinin elemanlarını yanyana string olarak yazdır (for of döngüsü kullanın)
 
+const arabalar = ["bmw", "mercedes", "audi", "volvo"]
+let araba1 = ""
+
+for (let item of arabalar) {
+    araba1 += item
+    // araba1=araba1.concat(item)   alternatif cozum
 }
-findAnimal()
+
+console.log(araba1);
+
+//! 2.yol
+const araba = arabalar.join("")
+console.log(araba);
