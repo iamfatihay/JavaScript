9a45fc998ec5eeb321b9b4402a6c1e47cc6041a5
Fatih <16fay61@gmail.com>
2023-04-13 11:53:50 +0200

    13.04.23

--- class-notes/DE-07-Arrays-Iteration/1-array-loop.js
+++ class-notes/DE-07-Arrays-Iteration/1-array-loop.js
@@ -21,7 +21,47 @@ const dizilereAyir1 =()=>{
     }
 console.log(negatifler1);
 console.log(pozitifler1);
-
-
 }
+
 dizilereAyir1()
+
+//* ======================================================
+//*                     FOR IN
+//* ======================================================
+const dizi2=[-5,15,22,-4,45,79,-54]
+
+const negatifler2=[]
+const pozitifler2=[]
+
+const dizilereAyir2 =()=>{
+
+    for (let i in dizi2) {
+        if (dizi2[i] < 0) {
+            negatifler2.push(dizi2[i])
+        }else pozitifler2.push(dizi2[i])     
+    }
+console.log(negatifler2);
+console.log(pozitifler2);
+}
+
+dizilereAyir2()
+//* ======================================================
+//*                     FOR OF
+//* ======================================================
+const dizi3=[-5,15,22,-4,45,79,-54]
+
+const negatifler3=[]
+const pozitifler3=[]
+
+const dizilereAyir3 =()=>{
+
+    for (let i of dizi3) {
+        if (i < 0) {
+            negatifler3.push(i)
+        }else pozitifler3.push(i)     
+    }
+console.log(negatifler3);
+console.log(pozitifler3);
+}
+
+dizilereAyir3()
