33992364825dcea90e3131fc499e2173bbbe3233
Fatih <16fay61@gmail.com>
2023-04-14 10:01:11 +0200

    aa

--- class-notes/DE-07-Arrays-Iteration/2-array-iteration.js
+++ class-notes/DE-07-Arrays-Iteration/2-array-iteration.js
@@ -33,9 +33,37 @@ console.log(prices);
 
 //* ======================================================
 //*                       MAP METHOD
-//!map yapılan değişikliği bir diziye atıp ve bunu döndürebilir,  forEach gibi tüketmez.
+//!map yapılan değişikliği bir diziye atıp , bunu döndürebilir,  forEach gibi tüketmez.
 //* ======================================================
 
 //*-------------- ÖRNEK -------------------
 //* names dizisinin içindeki her ismi büyük harf olarak
 //* yeni bir diziye saklayalım.
+
+const names=["kubilay","irfan","fatih","özlem","nihal","yunus","emirhan","halit","esra","hüseyin"]
+
+const yeniIsim = names.map((isim)=>isim.toUpperCase())
+
+console.log(yeniIsim);
+
+
+//*örnek
+//* fiyatlar dizisindeki ürün fiyatlarin 250 TL altında olanlarına
+//* %50 zam, diğerlerine de %20 zam yapılmak isteniyor. Ayrıca
+//* zamli fiyatlar kalıcı olarak diziye işlenmek isteniyor fiyatlar[1]=eski fiyat*1.5 gibi
+//* 1. ürünün zamlı fiyati 150 TL gibi
+//* p => prices dizisinin her bir elemanı
+//* i => indis
+//* array=> fiyatlar dizisidir.
+
+const fiyatlar = [100,250,50,90];
+
+fiyatlar.map((p,i,array)=>{
+    if (p < 250) {
+        array[i] = array[i] * 1.5
+    } else {
+        array[i] = array[i] * 1.2
+    }
+})
+
+console.log(fiyatlar);
