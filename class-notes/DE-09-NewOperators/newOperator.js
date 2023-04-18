0f15816c1f058e445750f0dfb1f296faa05fa1c8
Fatih <16fay61@gmail.com>
2023-04-18 11:10:52 +0200

    aa

--- class-notes/DE-09-NewOperators/newOperator.js
+++ class-notes/DE-09-NewOperators/newOperator.js
@@ -93,3 +93,88 @@ const people = [
       age: 24,
     },
   ];
+
+  people.forEach((kisi)=>{
+
+    // console.log(kisi.name);
+    // console.log(kisi.surname);
+    // console.log(kisi.job);
+    // console.log(kisi.age);
+    const{name,surname,job,age}=kisi
+    console.log(name);
+    console.log(surname);
+    console.log(job);
+    console.log(age);
+  })
+
+  //? Fonksiyonlar object olarak gelen parametreleri destructuring yapabilir.
+
+  const kontrol=(veri)=>{
+    // console.log(veri);
+    // console.log(veri.adi);
+    const{adi,renk,yildiz}=veri  //! destructuring
+    console.log(adi);
+    console.log(renk);
+    console.log(yildiz);
+  }
+
+  const data = {adi:"GS",renk:"sari-kirmizi",yildiz:4}
+  kontrol(data)
+  // **********************(2.tercih)
+
+  const kontrol2=({adi,renk,yildiz})=>{
+    console.log(adi);
+    console.log(renk);
+    console.log(yildiz);    
+  }
+
+  const data2 = {adi:"GS",renk:"sari-kirmizi",yildiz:4}
+  kontrol2(data2)
+
+//*======================================================
+//*   NEW GENERATION OPERATORS: SPREAD OR REST (...)
+//* ======================================================
+
+//? ------------------------------------------------------
+//?  REST =>>[...name]=diziden (ya da object den) alınmayan, geri kalanları yeni bir (diziyse) diziye,
+//? (object se)object e atıyor
+//? ------------------------------------------------------
+
+//! 1- Bir dizi veya object'deki bazi degerlerden geri kalanlarini
+//!    ayri dizi yada objelere kopyalanmasini saglayabilir.
+
+//*REST OBJECT***************************************
+
+const personel={
+    pName:"Johny",
+    surname:"DEEP",
+    job:"actor",
+    age:55
+}
+
+const{pName,age, ...gerisi}=personel
+console.log(pName);
+console.log(personel.job);
+console.log(gerisi);
+console.log(gerisi.job, personel.job);  // actor actor 
+
+//* OBJECT KOPYA
+//? Orjinal diziyle ayni sartlarla bir kopya olusturdum.
+const ikizPersonel=personel;
+
+//? orjinal diziden farkli bir kopya olusturduk
+const{...personel2}=personel
+console.log(personel2);
+
+
+//** orjinal diziden farkli bir kopya olusturdugumuz icin bu kopyaya yapilan degisiklik orjinal diziyi bozmaz */
+personel2.age=45
+// console.log(personel);
+// console.log(ikizPersonel);
+// console.log(personel2);
+
+//** orjinal diziyle ayni sartlarda bir kopya olustsurduk bu yuzden kopyada yapilan degisiklik orjinal diziyi de etkiledi */
+ikizPersonel.age=100
+console.log(personel);
+console.log(ikizPersonel);
+console.log(personel2);
