ededb3544706509d19476541f9a43553538701af
Fatih <16fay61@gmail.com>
2023-04-24 18:33:29 +0200

    aa

--- class-notes/DE-13-To-Do-App/app.js
+++ class-notes/DE-13-To-Do-App/app.js
@@ -5,13 +5,25 @@ let totalTask=0
 
 document.querySelector("#todo-button").onclick=()=>{
 
-    liste.innerHTML=`<li><div><i class="fa-solid fa-check fa-2xl"></i>${newTask.value}</div><div class="ikinci"><i class="fa-solid fa-trash fa-xl"></i></div></li>`+liste.innerHTML
+    liste.innerHTML=`<li><div class="birinci"><i class="fa-solid fa-check fa-2xl"></i></div>${newTask.value}<div class="ikinci"><i class="fa-solid fa-trash fa-xl"></i></div></li>`+liste.innerHTML
     //* ekle ye basinca input un ici tekrar bosalsin
     newTask.value=""
     totalTask++
     total.textContent=totalTask
-     
+    document.querySelectorAll(".ikinci").forEach((item) => {
+        item.onclick = () => {
+            (item.parentElement).remove() //alttaki yorum kisimlarina kisa yol olarak kullanilabilir
+        //   const li = item.parentElement;
+        //   li.remove(); 
+          totalTask--;
+          total.textContent = totalTask;
+        };
+      });
+      document.querySelectorAll(".birinci").forEach((item) => {
+        item.onclick = () => {
+            (item.parentElement).style.backgroundColor="lightgreen",
+            (item.parentElement).style.textDecoration = "line-through";
+        };
+      });        
 }
-document.querySelector(".ikinci").onclick=()=>{
-    liste.remove()
-} 
+
