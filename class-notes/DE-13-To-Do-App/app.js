9a90393326c4ef196381f290caec209d5b6f5106
Fatih <16fay61@gmail.com>
2023-04-26 12:06:16 +0200

    aa

--- class-notes/DE-13-To-Do-App/app.js
+++ class-notes/DE-13-To-Do-App/app.js
@@ -36,51 +36,78 @@
 //!  bu şekilde liste dizisine eklenen veri globalde old için içeriğine (includes) her yerden bakılabilir
 
 let liste = []
-let total=0
-let completed=0
+let total = 0
+let completed = 0
 
-const listeInput=document.querySelector("#todo-input")
-const listeUl=document.querySelector("#todo-ul")
-const listeButon=document.querySelector("#todo-button")
-const toplam=document.querySelector("#toplam")
+const listeInput = document.querySelector("#todo-input")
+const listeUl = document.querySelector("#todo-ul")
+const listeButon = document.querySelector("#todo-button")
+const toplam = document.querySelector("#toplam")
 
 //! add butonuna basildiginda:
-listeButon.onclick=()=>{
+listeButon.onclick = () => {
   if (!listeInput.value) {
     alert("Please enter something to do")
-  } else if(liste.includes(listeInput.value)){
+  } else if (liste.includes(listeInput.value)) {
     return
     // return=if in icindeki sart true ise hicbirsey yapma alt satirdan devam et
-  }else{
+  } else {
     liste.push(listeInput.value);
     total++
 
-    listeUl.innerHTML=`<li class="ayse">
+    listeUl.innerHTML = `<li class="ayse">
     <i class="fa-solid fa-check fa-lg"></i>
-    <p>${liste[liste.length-1]}</p>   //!<p>${listeInput.value}</p> bu da kullanilabilir
+    <p>${liste[liste.length - 1]}</p>   
     <i class="fa-solid fa-trash fa-lg"></i>
-    </li>`+listeUl.innerHTML;
-
-    toplam.textContent=total
-    listeInput.value=""
+    </li>`+ listeUl.innerHTML;
+    //!<p>${listeInput.value}</p> bu da kullanilabilir
+    toplam.textContent = total
+    listeInput.value = ""
     listeInput.focus() //! her li girisinde cursor inputta olsun 
   }
-//!silme metodu, ekrana basıldığı süslü de olmalı, dışarıda fa-trash lara tıklayınca,
-//! remove mantıklı değil, ortada fa-trash daha oluşmadan (add butonuna basılmadan) js ye çağırmak gibi oluyor
-// trash butonuna tıklanınca
-createSilButon()
+  //!silme metodu, ekrana basıldığı süslü de olmalı, dışarıda fa-trash lara tıklayınca,
+  //! remove mantıklı değil, ortada fa-trash daha oluşmadan (add butonuna basılmadan) js ye çağırmak gibi oluyor
+  // trash butonuna tıklanınca
+  createSilButon()
 
-//!check
-// createCheckButon()
+  //!check
+  createCheckButon()
 
 };
 
-const createSilButon=()=>{
+const createSilButon = () => {
   //? ul deki butun fa-trash lara ulas
-  document.querySelectorAll(".fa-trash").forEach((a)=>{
-    a.onclick=()=>{
-      
+  document.querySelectorAll(".fa-trash").forEach((a) => {
+    a.onclick = () => {
+      //! listeden silme
+      liste = liste.filter((yapilacak) => yapilacak != a.closest("li").querySelector("p").textContent) // li den sonra".innerText" ile de yaziya ulasabilirdik
+      // console.log(liste);
+
+      //! ekrandan silme, sadece ekrandan silersek,
+      //! sonrasinda diziyi bastirmaya kalkarsak eleman hala dizide bulundugu icin yanlis cevap aliriz,
+      a.parentElement.remove()
+      total--
+      toplam.textContent = total
     }
   })
 }
-// const createCheckButon=()=>{}
+const createCheckButon = () => {
+  //! Target özelliği, her zaman olay dinleyicisinin olayı tetiklediği öğeye başvuran currentTarget özelliğinin aksine,
+  //! olayın orjinal olarak gerçekleştiği öğeyi alır.
+  listeUl.onclick = (e) => {
+    // console.log(e.target.parentElement.className);
+    if (e.target.parentElement.classList.contains("checked")) {
+      e.target.parentElement.classList.remove("checked")
+      completed--
+    } else {
+      e.target.parentElement.classList.add("checked")
+      completed++
+    }
+    document.querySelector("#tamamlanan").textContent = completed
+  }
+  //! classList ve className sonuç istendiğinde aynı sonucu verir tek farkla;
+  //! classList bir liste, className bir isim şeklinde verir. bunun tek dezavantajı,
+  //! zaten class ı olan bir elemana className="örnek" şeklinde eleman atamak istersek,
+  //! varolan elemanları silip sadece örnek class ı nı atar. classList ile toggle ve contains kullanmalıyız,
+  //! className ile toggle kullanamıyoruz contains yerine includes kullanmalıyız, javascriptte includes tercih edilir
+}
