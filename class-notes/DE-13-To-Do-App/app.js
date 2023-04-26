9b1e246c6a7cc8678225b37f6b7991884506a7ec
Fatih <16fay61@gmail.com>
2023-04-26 10:03:24 +0200

    aa

--- class-notes/DE-13-To-Do-App/app.js
+++ class-notes/DE-13-To-Do-App/app.js
@@ -31,4 +31,36 @@
 //? *************** ALT TARAF HOCAYLA YAPILAN KISIM ******************************************
 //? ******************************************************************************************
 
-let liste=[]
+//! boş liste hem database işini kolaylaştırır, hem ul ye eklenen verinin aynısı tekrar eklenmesin demek için
+//! bütün p leri çağır içeriğinde input.value varsa ekleme, demeli ama p oluşmadan bunu diyemeyiz,
+//!  bu şekilde liste dizisine eklenen veri globalde old için içeriğine (includes) her yerden bakılabilir
+
+let liste = []
+let total=0
+let completed=0
+
+const listeInput=document.querySelector("#todo-input")
+const listeUl=document.querySelector("#todo-ul")
+const listeButon=document.querySelector("#todo-button")
+const toplam=document.querySelector("#toplam")
+
+//! add butonuna basildiginda:
+listeButon.onclick=()=>{
+  if (!listeInput.value) {
+    alert("Please enter something to do")
+  } else if(liste.includes(listeInput.value)){
+    return
+    // return=if in icindeki sart true ise hicbirsey yapma alt satirdan devam et
+  }else{
+    liste.push(listeInput.value);
+    total++
+
+    listeUl.innerHTML=`<li>
+    <i class="fa-solid fa-check fa-lg"></i>
+    <p>${liste[liste.length-1]}</p>   //!<p>${listeInput.value}</p> bu da kullanilabilir
+    <i class="fa-solid fa-trash fa-lg"></i>
+    </li>`+listeUl.innerHTML;
+
+    toplam.textContent=total
+  }
+}
