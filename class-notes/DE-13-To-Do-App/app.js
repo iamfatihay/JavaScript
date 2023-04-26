918c9637c081a97aacae1a95523f1cb1ca88868c
Fatih <16fay61@gmail.com>
2023-04-27 01:33:08 +0200

    aa

--- class-notes/DE-13-To-Do-App/app.js
+++ class-notes/DE-13-To-Do-App/app.js
@@ -1,5 +1,5 @@
 let liste = [];
-//! boş liste hem database işini kolaylaştırır, hem ul ye eklenen verinin aynısı tekrar eklenmesin demek için ,bütün p leri çağır içeriğinde input.value deki değer varsa ekleme, demeli ama p oluşmadan bunu diyemeyiz, bu şekilde liste dizisine eklenen veri globalde old için içeriğine (includes) her yerden bakılabilir
+//! boş liste hem database işini kolaylaştirir, hem ul ye eklenen verinin aynisi tekrar eklenmesin demek için ,bütün p leri çağir içeriğinde input.value deki değer varsa ekleme, demeli ama p oluşmadan bunu diyemeyiz, bu şekilde liste dizisine eklenen veri globalde old için içeriğine (includes) her yerden bakilabilir
 
 let total = 0;
 let completed = 0;
@@ -9,16 +9,16 @@ const listeUl = document.querySelector("#todo-ul");
 const listeButon = document.querySelector("#todo-button");
 const toplam = document.querySelector("#toplam");
 
-//! Add butonuna basıldığında
+//! Add butonuna basildiğinda
 
 listeButon.onclick = () => {
   if (!listeInput.value) {
     alert("lütfen bir not giriniz");
 
-    //! input a girilen veri (Add e basınca) daha önce listeye girildiyse, tekrar girilmesin, boş dön, bu kontrolü mesela ekrandaki li leri çekerek yapamayız, çünkü includes yada forEach ler dizi de çalışır
+    //! input a girilen veri (Add e basinca) daha önce listeye girildiyse, tekrar girilmesin, boş dön, bu kontrolü mesela ekrandaki li leri çekerek yapamayiz, çünkü includes yada forEach ler dizi de çalişir
   } else if (liste.includes(listeInput.value)) {
     return;
-    // return=if in içindeki şart true ise hiçbirşey yapma, koda alt satırdan devam et demek
+    // return=if in içindeki şart true ise hiçbirşey yapma, koda alt satirdan devam et demek
   } else {
     liste.push(listeInput.value);
 
@@ -35,91 +35,89 @@ listeButon.onclick = () => {
     //! 2. p ekrana basma yolu
     // <p>${listeInput.value}</p>
 
-    listeInput.value=""
+    listeInput.value = ""
 
     //? her li girişinde cursor inputta olsun
     listeInput.focus()
 
   }
 
-//!silme metodu, ekrana basıldığı süslü de olmalı, dışarıda fa-trash lara tıklayınca, remove mantıklı değil, ortada fa-trash daha oluşmadan (add butonuna basılmadan) js ye çağırmak gibi oluyor
-// trash butonuna tıklanınca
- createSilButon()
-// burada querySelectorAll yolunu tercih ettik
-//!check
-   createCheckButon()
-//burada da target yolunu tercih ettik
+  //!silme metodu, ekrana basildiği süslü de olmali, dişarida fa-trash lara tiklayinca, remove mantikli değil, ortada fa-trash daha oluşmadan (add butonuna basilmadan) js ye çağirmak gibi oluyor
+  // trash butonuna tiklaninca
+  createSilButon()
+  // burada querySelectorAll yolunu tercih ettik
+  //!check
+  createCheckButon()
+  //burada da target yolunu tercih ettik
 };
 
-const createSilButon=()=>{
+const createSilButon = () => {
 
-//?ul deki bütün fa-trash lara ulaş
-document.querySelectorAll(".fa-trash").forEach((a)=>{
+  //?ul deki bütün fa-trash lara ulaş
+  document.querySelectorAll(".fa-trash").forEach((a) => {
 
-    a.onclick=()=>{
+    a.onclick = () => {
 
-// console.log(a.parentElement);
-//!listeden silme
-// console.log(a.closest("li").querySelector("p").textContent);
-//! closest=herhangi bir elemanın istediğiniz uzaklıktaki parent ına götürür. yani a.parentElement.parentElement.parentElement ile 3. babaya ulaşabilirken closest ile 3. babanın adını yazarak direk ulaşabiliriz
+      // console.log(a.parentElement);
+      //!listeden silme
+      // console.log(a.closest("li").querySelector("p").textContent);
+      //! closest=herhangi bir elemanin istediğiniz uzakliktaki parent ina götürür. yani a.parentElement.parentElement.parentElement ile 3. babaya ulaşabilirken closest ile 3. babanin adini yazarak direk ulaşabiliriz
 
 
- liste=liste.filter((yapılacak)=>yapılacak!=a.closest("li").querySelector("p").textContent)
-console.log(liste);
-//!ekrandan silme, sadece ekrandan silersek sonrasında diziyi bastırmaya kalkarsak, eleman hala dizide bulunduğu için yanlış cevap alırız
-a.parentElement.remove()
-total=total-1
- toplam.textContent = total;
+      liste = liste.filter((yapilacak) => yapilacak != a.closest("li").querySelector("p").textContent)
+      console.log(liste);
+      //!ekrandan silme, sadece ekrandan silersek sonrasinda diziyi bastirmaya kalkarsak, eleman hala dizide bulunduğu için yanliş cevap aliriz
+      a.parentElement.remove()
+      total = total - 1
+      toplam.textContent = total;
 
-if( a.parentElement.classList.contains("checked"))
-{
-    completed=completed-1
-    document.querySelector("#tamamlanan").textContent = completed;
-}
+      if (a.parentElement.classList.contains("checked")) {
+        completed = completed - 1
+        document.querySelector("#tamamlanan").textContent = completed;
+      }
 
     }
-})
+  })
 
 }
 
- const createCheckButon=()=>{
-   //   //!2.yol
-   document.querySelectorAll(".fa-check").forEach((a) => {
-     a.onclick = () => {
-       if (a.parentElement.className == "checked") {
-         a.parentElement.className = "";
-         completed = completed - 1;
-       } else {
-         a.parentElement.className = "checked";
-         completed = completed + 1;
-       }
-       document.querySelector("#tamamlanan").textContent = completed;
-     };
-   });
-   //! 1.yol Target özelliği, her zaman olay dinleyicisinin olayı tetiklediği öğeye başvuran currentTarget özelliğinin aksine, olayın orjinal olarak gerçekleştiği öğeyi alır .
+const createCheckButon = () => {
+  //   //!2.yol
+  document.querySelectorAll(".fa-check").forEach((a) => {
+    a.onclick = () => {
+      if (a.parentElement.className == "checked") {
+        a.parentElement.className = "";
+        completed = completed - 1;
+      } else {
+        a.parentElement.className = "checked";
+        completed = completed + 1;
+      }
+      document.querySelector("#tamamlanan").textContent = completed;
+    };
+  });
+  //! 1.yol Target özelliği, her zaman olay dinleyicisinin olayi tetiklediği öğeye başvuran currentTarget özelliğinin aksine, olayin orjinal olarak gerçekleştiği öğeyi alir .
 
-   //    listeUl.onclick = (e) => {
-   //     console.log(e.target);
-   //      console.log(e.target.parentElement.classList);
+  //    listeUl.onclick = (e) => {
+  //     console.log(e.target);
+  //      console.log(e.target.parentElement.classList);
 
-   //      // if(e.target.parentElement.className=="checked"){
-   //      // e.target.parentElement.className="ayse"
+  //      // if(e.target.parentElement.className=="checked"){
+  //      // e.target.parentElement.className="ayse"
 
-   //      // }else{e.target.parentElement.className="checked"}
+  //      // }else{e.target.parentElement.className="checked"}
 
-   //      if (e.target.parentElement.classList.contains("checked")) {
-   //        e.target.parentElement.classList.remove("checked");
-   // completed=completed-1
+  //      if (e.target.parentElement.classList.contains("checked")) {
+  //        e.target.parentElement.classList.remove("checked");
+  // completed=completed-1
 
-   //      } else {
-   //        e.target.parentElement.classList.add("checked");
-   // completed = completed + 1;
+  //      } else {
+  //        e.target.parentElement.classList.add("checked");
+  // completed = completed + 1;
 
-   //      }
-   //      document.querySelector("#tamamlanan").textContent=completed
-   //    };
+  //      }
+  //      document.querySelector("#tamamlanan").textContent=completed
+  //    };
 
-   // !! classList ve className sonuç istendiğinde aynı sonucu verir tek farkla; classList bir liste, className bir isim şeklinde verir. bunun tek dezavantajı, zaten class ı olan bir elemana className="örnek" şeklinde eleman atamak istersek, varolan elemanları silip sadece örnek class ı nı atar. classList ile toggle ve contains kullanmalıyız, className ile toggle kullanamıyoruz contains yerine includes kullanmalıyız, javascriptte includes tercih edilir
- }
- 
-   
+  // !! classList ve className sonuç istendiğinde ayni sonucu verir tek farkla; classList bir liste, className bir isim şeklinde verir. bunun tek dezavantaji, zaten class i olan bir elemana className="örnek" şeklinde eleman atamak istersek, varolan elemanlari silip sadece örnek class i ni atar. classList ile toggle ve contains kullanmaliyiz, className ile toggle kullanamiyoruz contains yerine includes kullanmaliyiz, javascriptte includes tercih edilir
+}
+
--- class-notes/DE-14-CheckoutPage/app.js
+++ class-notes/DE-14-CheckoutPage/app.js
@@ -1,117 +1,110 @@
-let bagTotal = 25.98
-let bagNumber = 1
-const bag = document.querySelector("#bag")
+let bagPrice = 25.98;
+let bagNumber = 1;
+const bag = document.querySelector("#bag");
 
-let shoeTotal = 45.99
-let shoeNumber = 1
-const shoe = document.querySelector("#shoe")
+let shoePrice = 45.99;
+let shoeNumber = 1;
+const shoe = document.querySelector("#shoe");
 
-let clockTotal = 74.99
-let clockNumber = 1
-const clock = document.querySelector("#clock")
+let clockPrice = 74.99;
+let clockNumber = 1;
+const clock = document.querySelector("#clock");
 
-let subTotal = bagTotal + shoeTotal + clockTotal;
-let tax = subTotal * 0.18
+let subTotal = bagPrice + shoePrice + clockPrice;
+let tax = subTotal * 0.18;
 let total = subTotal + tax + 15; //shipping=15
-
-
-
-
-
-
-
-
-
-document.querySelector(".minus1").onclick = () => {
-    if (document.querySelector(".number1").textContent == 0) {
-        return
-    } else {
-        bagNumber--
-        document.querySelector(".number1").textContent = bagNumber
-        bagTotal-=25.98
-        bag.textContent=bagTotal.toFixed(2)
-        subTotal = bagTotal + shoeTotal + clockTotal
-        document.querySelector("#subTotal").textContent=subTotal.toFixed(2)
-        tax = subTotal * 0.18
-        document.querySelector("#tax").textContent=tax.toFixed(2)
-        total = subTotal + tax + 15
-        document.querySelector("#total").textContent=total.toFixed(2)
+ 
+function updatePrice() {
+    subTotal = bagPrice + shoePrice + clockPrice
+    document.querySelector("#subTotal").textContent = subTotal.toFixed(2)
+    tax = subTotal * 0.18
+    document.querySelector("#tax").textContent = tax.toFixed(2)
+    total = subTotal + tax + 15
+    document.querySelector("#total").textContent = total.toFixed(2)
+    if (bagNumber==0 && shoeNumber==0 && clockNumber==0) {
+        document.querySelector("#shipping").textContent="0.00"
+        total=0
+        document.querySelector("#total").textContent = total.toFixed(2);
     }
-}
-document.querySelector(".plus1").onclick = () => {
+  }
+  
+  document.querySelector(".minus1").onclick = () => {
+    if (document.querySelector(".number1").textContent == 0) {
+      return
+    } else {
+      bagNumber--
+      document.querySelector(".number1").textContent = bagNumber
+      bagPrice -= 25.98
+      bag.textContent = bagPrice.toFixed(2)
+      updatePrice()
+    }
+  }
+  
+  document.querySelector(".plus1").onclick = () => {
     bagNumber++
     document.querySelector(".number1").textContent = bagNumber
-    bagTotal=bagNumber*25.98
-    bag.textContent=bagTotal.toFixed(2)
-    subTotal = bagTotal + shoeTotal + clockTotal
-    document.querySelector("#subTotal").textContent=subTotal.toFixed(2)
-    tax = subTotal * 0.18
-    document.querySelector("#tax").textContent=tax.toFixed(2)
-    total = subTotal + tax + 15
-    document.querySelector("#total").textContent=total.toFixed(2)
-}
-
-document.querySelector(".minus2").onclick = () => {
+    bagPrice = bagNumber * 25.98
+    bag.textContent = bagPrice.toFixed(2)
+    updatePrice()
+  }
+  
+  document.querySelector(".minus2").onclick = () => {
     if (document.querySelector(".number2").textContent == 0) {
-        return
+      return
     } else {
-        shoeNumber--
-        document.querySelector(".number2").textContent = shoeNumber
-        shoeTotal-=45.99
-        shoe.textContent=shoeTotal.toFixed(2)
-        subTotal = bagTotal + shoeTotal + clockTotal
-        document.querySelector("#subTotal").textContent=subTotal.toFixed(2)
-        tax = subTotal * 0.18
-        document.querySelector("#tax").textContent=tax.toFixed(2)
-        total = subTotal + tax + 15
-        document.querySelector("#total").textContent=total.toFixed(2)
+      shoeNumber--
+      document.querySelector(".number2").textContent = shoeNumber
+      shoePrice -= 45.99
+      shoe.textContent = shoePrice.toFixed(2)
+      updatePrice()
     }
-}
-document.querySelector(".plus2").onclick = () => {
+  }
+  
+  document.querySelector(".plus2").onclick = () => {
     shoeNumber++
     document.querySelector(".number2").textContent = shoeNumber
-    shoeTotal=shoeNumber*45.99
-    shoe.textContent=shoeTotal.toFixed(2)
-    subTotal = bagTotal + shoeTotal + clockTotal
-    document.querySelector("#subTotal").textContent=subTotal.toFixed(2)
-    tax = subTotal * 0.18
-    document.querySelector("#tax").textContent=tax.toFixed(2)
-    total = subTotal + tax + 15
-    document.querySelector("#total").textContent=total.toFixed(2)
-}
-
-document.querySelector(".minus3").onclick = () => {
+    shoePrice = shoeNumber * 45.99
+    shoe.textContent = shoePrice.toFixed(2)
+    updatePrice()
+  }
+  
+  document.querySelector(".minus3").onclick = () => {
     if (document.querySelector(".number3").textContent == 0) {
-        return
+      return
     } else {
-        clockNumber--
-        document.querySelector(".number3").textContent = clockNumber
-        clockTotal-=74.99
-        clock.textContent=clockTotal.toFixed(2)
-        subTotal = bagTotal + shoeTotal + clockTotal
-        document.querySelector("#subTotal").textContent=subTotal.toFixed(2)
-        tax = subTotal * 0.18
-        document.querySelector("#tax").textContent=tax.toFixed(2)
-        total = subTotal + tax + 15
-        document.querySelector("#total").textContent=total.toFixed(2)
+      clockNumber--
+      document.querySelector(".number3").textContent = clockNumber
+      clockPrice -= 74.99
+      clock.textContent = clockPrice.toFixed(2)
+      updatePrice()
     }
-}
-document.querySelector(".plus3").onclick = () => {
+  }
+
+  document.querySelector(".plus3").onclick = () => {
     clockNumber++
     document.querySelector(".number3").textContent = clockNumber
-    clockTotal=clockNumber*74.99
-    clock.textContent=clockTotal.toFixed(2)
-    subTotal = bagTotal + shoeTotal + clockTotal
-    document.querySelector("#subTotal").textContent=subTotal.toFixed(2)
-    tax = subTotal * 0.18
-    document.querySelector("#tax").textContent=tax.toFixed(2)
-    total = subTotal + tax + 15
-    document.querySelector("#total").textContent=total.toFixed(2)
-}
-
+    clockPrice = clockNumber * 74.99
+    clock.textContent = clockPrice.toFixed(2)
+    updatePrice()
+  }
 
 document.querySelectorAll(".remove").forEach((item) => {
-    item.onclick=()=>{
-       (item.closest(".row")).remove()
+    item.onclick = () => { 
+        const row = item.closest(".row");
+        row.remove();
+        const rowAmount = parseFloat(row.querySelector(".sil").textContent);
+        subTotal=subTotal-rowAmount
+        document.querySelector("#subTotal").textContent = subTotal.toFixed(2)
+        tax = subTotal * 0.18
+        document.querySelector("#tax").textContent = tax.toFixed(2)
+        total = subTotal + tax + 15
+        document.querySelector("#total").textContent = total.toFixed(2);
+        if (total<17) {
+            document.querySelector("#shipping").textContent="0.00"
+            total=0
+            document.querySelector("#total").textContent = total.toFixed(2);
+        }
     }
-})
+});
+
+  
