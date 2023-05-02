fa285a1b43ff78cf3f8342d57568974ddfc91696
Fatih <16fay61@gmail.com>
2023-05-02 10:26:25 +0200

    aa

--- class-notes/DE-15-Async/1-timeout_interval.js
+++ class-notes/DE-15-Async/1-timeout_interval.js
@@ -18,3 +18,66 @@
 
 //*Senkron Programlama
 //****************************************** */
+
+// const bekle=(ms)=>{
+//     const start=new Date().getTime()
+//     while(new Date().getTime()<start+ms){
+        
+//     }
+    
+//     }
+console.log("hello");
+// console.time("timer1")//kronometreyi başlat
+// bekle(3000)
+// console.timeEnd("timer1")//kronometreyi durdur
+
+//*Asenkron (setTimeout) 1 seferlik
+//******************************** */
+
+// setTimeout(()=>{
+
+//     console.log("selamun aleyküm");
+
+// },2000)
+// console.log("aleyküm selam");
+
+// setTimeout(() => {
+//   console.log("naber napiyorsun");
+// }, 3000);
+
+//!Aleyküm Selam-Selamun Aleyküm- Naber napiyorsun çalişir, 3. nün süresi 1.den azsa, 2-3-1 sirasinda çalişir
+//!settimeout müsait zamanda ortaya çikar, onun sirasi geçtiğinde alttaki işlemler bitince çalişir
+
+//*Asenkron(setInterval, clearInterval) belli aralıklarla durdurulana kadar devam eder.
+//* mesela 1 dakikada bir döviz kurunu güncellemek için kullanılabilir
+//***************************** */
+
+//?setInterval non-blocking
+//? ***************************** /
+
+
+console.log("sayac basliyor");
+let sayac=0
+
+const interval1= setInterval(()=>{
+
+    console.log(++sayac);
+    if (sayac==5) {
+        clearInterval(interval1)
+    }
+},1000)
+
+console.log("sayac durduruldu");
+
+// //?callback hell
+//? ***************************** /
+
+setTimeout(() => {
+    console.log("ayse:s.a. ahmet");
+    setTimeout(() => {
+      console.log("ahmet:a.s. ayse nasılsın");
+      setTimeout(() => {
+        console.log("ayse:iyiyim sağol");
+      }, 1000);
+    }, 1000);
+  }, 1000);
