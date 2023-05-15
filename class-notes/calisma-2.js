dc36497cf12b80d287c2dd7c36a44d7a23a2ae4f
Fatih <16fay61@gmail.com>
2023-05-15 18:50:44 +0200

    aa

--- class-notes/calisma-2.js
+++ class-notes/calisma-2.js
@@ -66,12 +66,45 @@
 // cat.type = 'tiger';
 // console.log(copyCat.type, copyCat.size);
 
-console.log(sum(10, 20)); 
-diff(10, 20);
-function sum(x, y) {
-  return x + y;
+// console.log(sum(10, 20)); 
+// diff(10, 20);
+// function sum(x, y) {
+//   return x + y;
+// }
+
+// let diff = function (x, y) {
+//   return x - y;
+// };
+
+
+// let arr = [1,3,-1,-3,5,3,6,7]
+// let result=[]
+// const k=3
+
+// for (let i = 0; i < array.length-(k-1); i++) {
+  
+//   let maks = Math.max(...array.slice(i,k+i))
+//   result.push(maks)
+
+// }
+// console.log(result);
+
+let arr = [1,3,-1,-3,5,3,6,7]
+
+function takeMax(array) {
+  let result=[] 
+  let k = +prompt("Please enter a number: ")
+
+  if (k < 1 || k > array.length) {
+    console.log("Invalid value!");
+    return;
+  }
+
+  for (let i = 0; i < array.length-(k-1); i++) {
+    let maks = Math.max(...array.slice(i,k+i))
+    result.push(maks)
+  }
+  console.log(result);
 }
 
-let diff = function (x, y) {
-  return x - y;
-};
+takeMax(arr)
